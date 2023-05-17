c1062f934b6a0f0a9f36a27ea893ec65fa0e367c
Md. Tausif Hossain <tausif.iub@gmail.com>
2023-05-17 11:38:43 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,44 @@
+function function_1()
+{
+    for(var i = 0; i<4; i++)
+    {
+        console.log('JS')
+    }
+    console.log(i);
+}
+
+
+function function_2()
+{
+    for(let i = 0; i<4; i++)
+    {
+        console.log('JS')
+    }
+    console.log(i);
+}
+
+
+function function_3()
+{
+    for(const i = 0; i<4; i++)
+    {
+        console.log('JS')
+    }
+    console.log(i);
+}
+
+// function_1();
+// function_2();
+// function_3();
+
+// var a = 10;
+// var a = 20;
+// console.log(a);
+
+// let a = 10;
+// let a = 20;
+// console.log(a);
+
+// const a = 10;
+// const a = 20;
+// console.log(a);
